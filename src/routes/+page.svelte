aa6960d85a88ebadc0065a46dcf8d870299eef7c
Thatderpypotat <idajac@edu.umea.se>
2024-04-09 11:05:07 +0200

    prisma and info

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,14 +2,11 @@
 
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
+		<h1 class="h1">Välkommen!</h1>
+		<p>Börja med att registrera ett konto eller logga in:</p>
 		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
+			<li><a href="/Login">Logga in med existerande konto</a></li>
+			<li><a href="/Registrera">Registrera ett nytt konto</a></li>
 		</ul>
 	</div>
 </div>
